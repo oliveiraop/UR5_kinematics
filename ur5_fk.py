f7d7b32ecce23693db86724019d09cc8c1bbf6bf
Joao Carneiro <jvmcarneiro@gmail.com>
2022-06-27 12:56:12 -0300

    Termina IK com teste

--- ur5_fk.py
+++ ur5_fk.py
@@ -1,34 +1,66 @@
 # Teste de 3 posicoes do UR5 via Cinematica Direta (angulo nas juntas).
-# Por Joao Victor Carneiro
+# Por:
+# Joao Victor Carneiro
+# Osmar Oliveira
 
 import numpy as np
+
 from zmqRemoteApi import RemoteAPIClient
+
 
 # Funcao de Cinematica Direta (utilizando parametros DH extraidos do cenario)
 def ur5_fk(theta_list):
-    a = [0, 0, -0.7391+0.314, -1.1312+0.7391, 0, 0]
-    alpha = [0, np.pi/2, 0, 0, np.pi/2, -np.pi/2]
-    d = [0.4992-0.41, 0, 0, -0.1038+0.2140, 0.4994-0.4047, -0.0288+0.1038]
-    theta = [theta_list[0], theta_list[1], theta_list[2], theta_list[3], theta_list[4], theta_list[5]]
+    a = [0, 0, -0.7391 + 0.314, -1.1312 + 0.7391, 0, 0]
+    alpha = [0, np.pi / 2, 0, 0, np.pi / 2, -np.pi / 2]
+    d = [0.4992 - 0.41, 0, 0, -0.1038 + 0.2140, 0.4994 - 0.4047, -0.0288 + 0.1038]
+    theta = [
+        theta_list[0],
+        theta_list[1],
+        theta_list[2],
+        theta_list[3],
+        theta_list[4],
+        theta_list[5],
+    ]
 
     # Transformacao homogenea do sistema de coordenadas original ate a base do UR5
-    r_base = np.array([[np.cos(np.pi/2), -np.sin(np.pi/2), 0],
-                       [np.sin(np.pi/2), np.cos(np.pi/2), 0],
-                       [0, 0, 1]])
+    r_base = np.array(
+        [
+            [np.cos(np.pi / 2), -np.sin(np.pi / 2), 0],
+            [np.sin(np.pi / 2), np.cos(np.pi / 2), 0],
+            [0, 0, 1],
+        ]
+    )
     d_base = np.array([[-0.214, -0.314, 0.41]])
     last_row = np.array([[0, 0, 0, 1]])
-    h_base = np.concatenate((np.concatenate((r_base, d_base.T), axis=1), last_row), axis=0)
+    h_base = np.concatenate(
+        (np.concatenate((r_base, d_base.T), axis=1), last_row), axis=0
+    )
 
     # Multiplica as transformacoes homogeneas entre cada junta
     t_matrix = h_base
     for i in np.arange(6):
-        a_matrix = np.array([[np.cos(theta[i]), -np.sin(theta[i]), 0, a[i]],
-                             [np.sin(theta[i])*np.cos(alpha[i]), np.cos(theta[i])*np.cos(alpha[i]), -np.sin(alpha[i]), -np.sin(alpha[i])*d[i]],
-                             [np.sin(theta[i])*np.sin(alpha[i]), np.cos(theta[i])*np.sin(alpha[i]), np.cos(alpha[i]), np.cos(alpha[i])*d[i]],
-                             [0, 0, 0, 1]])
+        a_matrix = np.array(
+            [
+                [np.cos(theta[i]), -np.sin(theta[i]), 0, a[i]],
+                [
+                    np.sin(theta[i]) * np.cos(alpha[i]),
+                    np.cos(theta[i]) * np.cos(alpha[i]),
+                    -np.sin(alpha[i]),
+                    -np.sin(alpha[i]) * d[i],
+                ],
+                [
+                    np.sin(theta[i]) * np.sin(alpha[i]),
+                    np.cos(theta[i]) * np.sin(alpha[i]),
+                    np.cos(alpha[i]),
+                    np.cos(alpha[i]) * d[i],
+                ],
+                [0, 0, 0, 1],
+            ]
+        )
         t_matrix = np.matmul(t_matrix, a_matrix)
     t_matrix[abs(t_matrix) < 1e-15] = 0
     return t_matrix
+
 
 # Altera posicoes das juntas
 def set_pos(theta_list):
@@ -36,41 +68,85 @@ def set_pos(theta_list):
     vel = 20
     accel = 40
     jerk = 20
-    current_vel = [0,0,0,0,0,0,0]
-    current_accel = [0,0,0,0,0,0,0]
-    max_vel = [vel*np.pi/180,vel*np.pi/180,vel*np.pi/180,vel*np.pi/180,vel*np.pi/180,vel*np.pi/180]
-    max_accel = [accel*np.pi/180,accel*np.pi/180,accel*np.pi/180,accel*np.pi/180,accel*np.pi/180,accel*np.pi/180]
-    max_jerk = [jerk*np.pi/180,jerk*np.pi/180,jerk*np.pi/180,jerk*np.pi/180,jerk*np.pi/180,jerk*np.pi/180]
-    target_vel = [0,0,0,0,0,0]
+    current_vel = [0, 0, 0, 0, 0, 0, 0]
+    current_accel = [0, 0, 0, 0, 0, 0, 0]
+    max_vel = [
+        vel * np.pi / 180,
+        vel * np.pi / 180,
+        vel * np.pi / 180,
+        vel * np.pi / 180,
+        vel * np.pi / 180,
+        vel * np.pi / 180,
+    ]
+    max_accel = [
+        accel * np.pi / 180,
+        accel * np.pi / 180,
+        accel * np.pi / 180,
+        accel * np.pi / 180,
+        accel * np.pi / 180,
+        accel * np.pi / 180,
+    ]
+    max_jerk = [
+        jerk * np.pi / 180,
+        jerk * np.pi / 180,
+        jerk * np.pi / 180,
+        jerk * np.pi / 180,
+        jerk * np.pi / 180,
+        jerk * np.pi / 180,
+    ]
+    target_vel = [0, 0, 0, 0, 0, 0]
 
-    # Aplica angulos nas juntas, compensando a diferenca na posicao inicial do cenario 
-    target_pos=[theta_list[0], theta_list[1]+np.pi/2, theta_list[2], theta_list[3]+np.pi/2, theta_list[4], theta_list[5]]
-    sim.rmlMoveToJointPositions(joint_handles,-1,current_vel,current_accel,max_vel,max_accel,max_jerk,target_pos,target_vel)
+    # Aplica angulos nas juntas, compensando a diferenca na posicao inicial do cenario
+    target_pos = [
+        theta_list[0],
+        theta_list[1] + np.pi / 2,
+        theta_list[2],
+        theta_list[3] + np.pi / 2,
+        theta_list[4],
+        theta_list[5],
+    ]
+    sim.rmlMoveToJointPositions(
+        joint_handles,
+        -1,
+        current_vel,
+        current_accel,
+        max_vel,
+        max_accel,
+        max_jerk,
+        target_pos,
+        target_vel,
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Inicia cliente e salva instancia do simulador atraves de API ZMQ
     client = RemoteAPIClient()
-    sim = client.getObject('sim')
+    sim = client.getObject("sim")
 
     # Inicia simulacao
     sim.startSimulation()
 
     # Salva referencia das juntas
-    joint1 = sim.getObject('/UR5/UR5_joint1')
-    joint2 = sim.getObject('/UR5/UR5_joint2')
-    joint3 = sim.getObject('/UR5/UR5_joint3')
-    joint4 = sim.getObject('/UR5/UR5_joint4')
-    joint5 = sim.getObject('/UR5/UR5_joint5')
-    joint6 = sim.getObject('/UR5/UR5_joint6')
+    joint1 = sim.getObject("/UR5/UR5_joint1")
+    joint2 = sim.getObject("/UR5/UR5_joint2")
+    joint3 = sim.getObject("/UR5/UR5_joint3")
+    joint4 = sim.getObject("/UR5/UR5_joint4")
+    joint5 = sim.getObject("/UR5/UR5_joint5")
+    joint6 = sim.getObject("/UR5/UR5_joint6")
 
-    # Teste 1
-    print("Teste 1 (theta_i = 0):")
-    tool = sim.getObject('/UR5/UR5_connection')
-    test1_angles = [0, 0, 0, 0, 0, 0]
-    print(f"t_calculado =\n{ur5_fk(test1_angles)}")
-    set_pos(test1_angles)
+    # Teste
+    print("Teste FK:")
+    tool = sim.getObject("/UR5/UR5_connection")
+    test_angles = [0, 0, 0, 0, 0, 0]
+    # test_angles = [-np.pi/4, -np.pi/4, -np.pi/4, -np.pi/4, -np.pi/4, -np.pi/4]
+    # test_angles = [-np.pi/2, -np.pi/2, -np.pi/2, -np.pi/2, -np.pi/2, -np.pi/2]
+    print(f"theta_input = {test_angles}")
+    t_calc = ur5_fk(test_angles)
+    print(f"t_calculado =\n{t_calc}")
+    print(f"p_calculado = {[np.round(x,4) for x in t_calc[:3,3].tolist()]}")
+    set_pos(test_angles)
     sim.wait(3)
     tool_position = sim.getObjectPosition(tool, sim.handle_world)
-    print(f"posicao_garra_sim = {tool_position}")
+    print(f"p_simulado = {[np.round(x,4) for x in tool_position]}")
 
-    sim.stopSimulation()
+    # sim.stopSimulation()
--- ur5_ik.py
+++ ur5_ik.py
@@ -3,173 +3,288 @@
 
 import numpy as np
 from numpy import cos, sin
+
 from zmqRemoteApi import RemoteAPIClient
-
-
-# Normaliza angulos para intervalo [0, 2pi]
-def normalize_angle(angle):
-    new_angle = np.arctan2(np.sin(angle), np.cos(angle))
-    if new_angle < 0:
-        new_angle = abs(new_angle) + 2 * (np.pi - abs(new_angle))
-    return new_angle
 
 
 # Gera matriz de transformacao de uma junta utilizando tabela DH
 def get_t_dh(joint, theta):
-    a = [0, 0, -0.7391+0.314, -1.1312+0.7391, 0, 0]
-    alpha = [0, np.pi/2, 0, 0, np.pi/2, -np.pi/2]
-    d = [0.4992-0.41, 0, 0, -0.1038+0.2140, 0.4994-0.4047, -0.0288+0.1038]
-    cur_a = a[joint-1]
-    cur_alpha = alpha[joint-1]
-    cur_d = d[joint-1]
+    a = [0, 0, -0.7391 + 0.314, -1.1312 + 0.7391, 0, 0]
+    alpha = [0, np.pi / 2, 0, 0, np.pi / 2, -np.pi / 2]
+    d = [0.4992 - 0.41, 0, 0, -0.1038 + 0.2140, 0.4994 - 0.4047, -0.0288 + 0.1038]
+    cur_a = a[joint - 1]
+    cur_alpha = alpha[joint - 1]
+    cur_d = d[joint - 1]
     t_dh = np.array(
-        [[np.cos(theta), -np.sin(theta), 0, cur_a],
-         [np.sin(theta)*np.cos(cur_alpha), np.cos(theta)*np.cos(cur_alpha), -np.sin(cur_alpha), -np.sin(cur_alpha)*cur_d],
-         [np.sin(theta)*np.sin(cur_alpha), np.cos(theta)*np.sin(cur_alpha), np.cos(cur_alpha), np.cos(cur_alpha)*cur_d],
-         [0, 0, 0, 1]])
+        [
+            [np.cos(theta), -np.sin(theta), 0, cur_a],
+            [
+                np.sin(theta) * np.cos(cur_alpha),
+                np.cos(theta) * np.cos(cur_alpha),
+                -np.sin(cur_alpha),
+                -np.sin(cur_alpha) * cur_d,
+            ],
+            [
+                np.sin(theta) * np.sin(cur_alpha),
+                np.cos(theta) * np.sin(cur_alpha),
+                np.cos(cur_alpha),
+                np.cos(cur_alpha) * cur_d,
+            ],
+            [0, 0, 0, 1],
+        ]
+    )
     return t_dh
 
 
 # Funcao de Cinematica Inversa do UR5 (utilizando posicao e orientacao)
-def ur5_ik(t_o_0, t_o_6, d6):
+def ur5_ik(t_o_0, t_o_6):
+    def r(num):
+        return np.around(num, 14)
+
+    # Distancia entre junta 6 e posicao da garra
+    d6 = 0.075
+
     t_0_6 = np.linalg.inv(t_o_0) @ t_o_6
+
     d4 = 0.1102
 
-    theta = np.zeros((6,8))
+    theta = np.empty((8, 6))
 
     # Theta 1
     p_0_5 = np.matmul(t_0_6, np.array([[0, 0, -d6, 1]]).T)
-    phi1 = np.arctan2(p_0_5[1,0], p_0_5[0,0])
-    phi2 = np.arccos(d4/np.sqrt(p_0_5[0,0]**2 + p_0_5[1,0]**2))
-    result1 = phi1 + phi2 + np.pi/2
-    result2 = phi1 - phi2 + np.pi/2
-    theta[0,:4] = normalize_angle(result1)
-    theta[0,4:] = normalize_angle(result2)
-    theta = np.real(theta)
+    phi1 = np.arctan2(r(p_0_5[1, 0]), r(p_0_5[0, 0]))
+    phi2 = np.arccos(r(r(d4) / r(np.sqrt(p_0_5[0, 0] ** 2 + p_0_5[1, 0] ** 2))))
+    result1 = phi1 + phi2 + np.pi / 2
+    result2 = phi1 - phi2 + np.pi / 2
+    theta[0:4, 0] = result1  # Ombro esquerda
+    theta[4:8, 0] = result2  # Ombro direita
 
     # Theta 5
-    p_0_6 = t_0_6[:,3]
-    m = 1
-    for i in np.arange(0,8,2):
-        result = m*np.arccos((p_0_6[0]*sin(theta[0,i])-p_0_6[1]*cos(theta[0,i]) - d4) / d6)
-        theta[4,i:i+2] = normalize_angle(result)
-        m *= -1
-    theta = np.real(theta)
+    for i in np.arange(0, 8, 4):
+        p_0_6 = t_0_6[:, 3]
+        result1 = np.arccos(
+            r(r((p_0_6[0] * sin(theta[i, 0]) - p_0_6[1] * cos(theta[i, 0])) - d4) / d6)
+        )
+        result2 = -np.arccos(
+            r(r((p_0_6[0] * sin(theta[i, 0]) - p_0_6[1] * cos(theta[i, 0]) - d4)) / d6)
+        )
+        theta[i : i + 2, 4] = result1  # Punho cima
+        theta[i + 2 : i + 4, 4] = result2  # Punho baixo
 
     # Theta 6
-    t_6_0 = np.linalg.inv(t_0_6)
-    x_6_0_x = t_6_0[0, 0]
-    x_6_0_y = t_6_0[1, 0]
-    y_6_0_x = t_6_0[0, 1]
-    y_6_0_y = t_6_0[1, 1]
-    for i in np.arange(8):
-        arg1 = (-x_6_0_y*sin(theta[0,i])+y_6_0_y*cos(theta[0,i]))/sin(theta[4,i])
-        arg2 = (x_6_0_x*sin(theta[0,i])-y_6_0_x*cos(theta[0,i]))/sin(theta[4,i])
-        theta[5,i] = normalize_angle(np.arctan2(arg1, arg2))
-    theta = np.real(theta)
+    for i in np.arange(0, 8, 2):
+        if abs(theta[i, 4]) == 0.0:
+            theta[i, 5] = 0.0
+        else:
+            t_6_0 = np.linalg.inv(t_0_6)
+            x_6_0_x = t_6_0[0, 0]
+            x_6_0_y = t_6_0[1, 0]
+            y_6_0_x = t_6_0[0, 1]
+            y_6_0_y = t_6_0[1, 1]
+            arg1 = (-x_6_0_y * sin(theta[i, 0]) + y_6_0_y * cos(theta[i, 0])) / sin(
+                theta[i, 4]
+            )
+            arg2 = (x_6_0_x * sin(theta[i, 0]) - y_6_0_x * cos(theta[i, 0])) / sin(
+                theta[i, 4]
+            )
+            theta[i : i + 2, 5] = np.arctan2(r(arg1), r(arg2))
 
     # Theta 3
-    for i in np.arange(0,8,2):
-        t_0_1 = get_t_dh(1, theta[0,i])
-        t_4_5 = get_t_dh(5, theta[4,i])
-        t_5_6 = get_t_dh(6, theta[5,i])
+    for i in np.arange(0, 8, 2):
+        t_0_1 = get_t_dh(1, theta[i, 0])
+        t_4_5 = get_t_dh(5, theta[i, 4])
+        t_5_6 = get_t_dh(6, theta[i, 5])
         t_1_6 = np.linalg.inv(t_0_1) @ t_0_6
         t_6_4 = np.linalg.inv(t_4_5 @ t_5_6)
         t_1_4 = t_1_6 @ t_6_4
-        p_1_4_xz = [[t_1_4[0,3], t_1_4[2,3]]]
-        result = np.arccos((np.linalg.norm(p_1_4_xz)**2-0.4251**2-0.3921**2)/(2*0.4251*0.3921))
-        theta[2,i] = normalize_angle(result)
-        theta[2,i+1] = normalize_angle(-result)
-    theta = np.real(theta)
+        p_1_4_xz = [[r(t_1_4[0, 3]), r(t_1_4[2, 3])]]
+        result1 = np.arccos(
+            r(
+                r((np.linalg.norm(p_1_4_xz) ** 2 - 0.4251**2 - 0.3921**2))
+                / r((2 * 0.4251 * 0.3921))
+            )
+        )
+        result2 = -np.arccos(
+            r(
+                r((np.linalg.norm(p_1_4_xz) ** 2 - 0.4251**2 - 0.3921**2))
+                / r((2 * 0.4251 * 0.3921))
+            )
+        )
+        theta[i, 2] = result1  # Cotovelo cima
+        theta[i + 1, 2] = result2  # Cotovelo baixo
 
     # Theta 2
-    for i in np.arange(8):
-        t_0_1 = get_t_dh(1, theta[0,i])
-        t_4_5 = get_t_dh(5, theta[4,i])
-        t_5_6 = get_t_dh(6, theta[5,i])
+    for i in np.arange(0, 8):
+        t_0_1 = get_t_dh(1, theta[i, 0])
+        t_4_5 = get_t_dh(5, theta[i, 4])
+        t_5_6 = get_t_dh(6, theta[i, 5])
         t_1_6 = np.linalg.inv(t_0_1) @ t_0_6
         t_6_4 = np.linalg.inv(t_4_5 @ t_5_6)
         t_1_4 = t_1_6 @ t_6_4
-        p_1_4_x = t_1_4[0,3]
-        p_1_4_z = t_1_4[2,3]
+        p_1_4_x = t_1_4[0, 3]
+        p_1_4_z = t_1_4[2, 3]
         p_1_4_xz = [[p_1_4_x, p_1_4_z]]
-        result = np.arctan2(-p_1_4_z, -p_1_4_x) - np.arcsin((0.3921*sin(theta[2,i]))/np.linalg.norm(p_1_4_xz))
-        theta[1,i] = normalize_angle(result)
+        result = np.arctan2(r(-p_1_4_z), r(-p_1_4_x)) - np.arcsin(
+            r(r((0.3921 * sin(theta[i, 2]))) / r(np.linalg.norm(p_1_4_xz)))
+        )
+        theta[i, 1] = result
 
     # Theta 4
-        t_1_2 = get_t_dh(2, theta[1,i])
-        t_2_3 = get_t_dh(3, theta[2,i])
+    for i in np.arange(0, 8):
+        t_0_1 = get_t_dh(1, theta[i, 0])
+        t_4_5 = get_t_dh(5, theta[i, 4])
+        t_5_6 = get_t_dh(6, theta[i, 5])
+        t_1_6 = np.linalg.inv(t_0_1) @ t_0_6
+        t_6_4 = np.linalg.inv(t_4_5 @ t_5_6)
+        t_1_4 = t_1_6 @ t_6_4
+        t_1_2 = get_t_dh(2, theta[i, 1])
+        t_2_3 = get_t_dh(3, theta[i, 2])
         t_1_3 = t_1_2 @ t_2_3
         t_3_4 = np.linalg.inv(t_1_3) @ t_1_4
-        x_3_4_y = t_3_4[1,0]
-        x_3_4_x = t_3_4[0,0]
-        result = np.arctan2(x_3_4_y, x_3_4_x)
-        theta[3,i] = normalize_angle(result)
-    theta = np.real(theta)
+        x_3_4_y = t_3_4[1, 0]
+        x_3_4_x = t_3_4[0, 0]
+        result = np.arctan2(r(x_3_4_y), r(x_3_4_x))
+        theta[i, 3] = result
 
+    # theta_drop_nan = theta[~np.isnan(theta).any(axis=1)]
     return theta
 
 
 # Gera matriz de transformacao a partir de posicao e orientacao
 def get_t_matrix(pos, ori):
-    [a, b, g] = ori[0]
-    r_x_a = np.array([[1, 0, 0],
-                      [0, cos(a), -sin(a)],
-                      [0, sin(a), cos(a)]])
-    r_y_b = np.array([[cos(b), 0, sin(b)],
-                      [0, 1, 0],
-                      [-sin(b), 0, cos(b)]])
-    r_z_g = np.array([[cos(g), -sin(g), 0],
-                      [sin(g), cos(g), 0],
-                      [0, 0, 1]])
+    [alpha, beta, gamma] = ori[0]
+    r_x_a = np.array(
+        [[1, 0, 0], [0, cos(alpha), -sin(alpha)], [0, sin(alpha), cos(alpha)]]
+    )
+    r_y_b = np.array([[cos(beta), 0, sin(beta)], [0, 1, 0], [-sin(beta), 0, cos(beta)]])
+    r_z_g = np.array(
+        [[cos(gamma), -sin(gamma), 0], [sin(gamma), cos(gamma), 0], [0, 0, 1]]
+    )
     r_total = r_x_a @ r_y_b @ r_z_g
     last_row = np.array([[0, 0, 0, 1]])
-    t_matrix = np.concatenate((np.concatenate((r_total, pos.T), axis=1), last_row), axis=0)
+    t_matrix = np.concatenate(
+        (np.concatenate((r_total, pos.T), axis=1), last_row), axis=0
+    )
     return t_matrix
 
+
 # Altera posicoes das juntas
-def set_pos(theta_list):
-    joint1 = sim.getObject('/UR5/UR5_joint1')
-    joint2 = sim.getObject('/UR5/UR5_joint2')
-    joint3 = sim.getObject('/UR5/UR5_joint3')
-    joint4 = sim.getObject('/UR5/UR5_joint4')
-    joint5 = sim.getObject('/UR5/UR5_joint5')
-    joint6 = sim.getObject('/UR5/UR5_joint6')
-    joint_handles = [joint1, joint2, joint3, joint4, joint5, joint6]
+def set_pos(joint_handles, theta_list):
     vel = 20
     accel = 40
     jerk = 20
-    current_vel = [0,0,0,0,0,0,0]
-    current_accel = [0,0,0,0,0,0,0]
-    max_vel = [vel*np.pi/180,vel*np.pi/180,vel*np.pi/180,vel*np.pi/180,vel*np.pi/180,vel*np.pi/180]
-    max_accel = [accel*np.pi/180,accel*np.pi/180,accel*np.pi/180,accel*np.pi/180,accel*np.pi/180,accel*np.pi/180]
-    max_jerk = [jerk*np.pi/180,jerk*np.pi/180,jerk*np.pi/180,jerk*np.pi/180,jerk*np.pi/180,jerk*np.pi/180]
-    target_vel = [0,0,0,0,0,0]
-    target_pos=[theta_list[0], theta_list[1]+np.pi/2, theta_list[2], theta_list[3]+np.pi/2, theta_list[4], theta_list[5]]
-    sim.rmlMoveToJointPositions(joint_handles,-1,current_vel,current_accel,max_vel,max_accel,max_jerk,target_pos,target_vel)
+    current_vel = [0, 0, 0, 0, 0, 0, 0]
+    current_accel = [0, 0, 0, 0, 0, 0, 0]
+    max_vel = [
+        vel * np.pi / 180,
+        vel * np.pi / 180,
+        vel * np.pi / 180,
+        vel * np.pi / 180,
+        vel * np.pi / 180,
+        vel * np.pi / 180,
+    ]
+    max_accel = [
+        accel * np.pi / 180,
+        accel * np.pi / 180,
+        accel * np.pi / 180,
+        accel * np.pi / 180,
+        accel * np.pi / 180,
+        accel * np.pi / 180,
+    ]
+    max_jerk = [
+        jerk * np.pi / 180,
+        jerk * np.pi / 180,
+        jerk * np.pi / 180,
+        jerk * np.pi / 180,
+        jerk * np.pi / 180,
+        jerk * np.pi / 180,
+    ]
+    target_vel = [0, 0, 0, 0, 0, 0]
+    target_pos = [
+        theta_list[0],
+        theta_list[1] + np.pi / 2,
+        theta_list[2],
+        theta_list[3] + np.pi / 2,
+        theta_list[4],
+        theta_list[5],
+    ]
+    sim.rmlMoveToJointPositions(
+        joint_handles,
+        -1,
+        current_vel,
+        current_accel,
+        max_vel,
+        max_accel,
+        max_jerk,
+        target_pos,
+        target_vel,
+    )
+
+
+# Reseta posicao da garra
+def reset_joints():
+    joint1 = sim.getObject("/UR5/UR5_joint1")
+    joint2 = sim.getObject("/UR5/UR5_joint2")
+    joint3 = sim.getObject("/UR5/UR5_joint3")
+    joint4 = sim.getObject("/UR5/UR5_joint4")
+    joint5 = sim.getObject("/UR5/UR5_joint5")
+    joint6 = sim.getObject("/UR5/UR5_joint6")
+    joint_handles = [joint1, joint2, joint3, joint4, joint5, joint6]
+    set_pos(joint_handles, [0, 0, 0, 0, 0, 0])
+    return joint_handles
+
+
+# Teste
 
 # Inicia cliente e salva instancia do simulador atraves de API ZMQ
 client = RemoteAPIClient()
-sim = client.getObject('sim')
+sim = client.getObject("sim")
 
-# Inicia simulacao
-sim.startSimulation()
-
-# Reseta posicao da garra
-set_pos([0,0,0,0,0,0])
-
-# Encontra matriz de transformacao da base para o copo
+# Encontra matriz de transformacao da base para a pos final
 base_pos = np.array([[-0.214, -0.314, 0.41]])
-base_ori = np.array([[0, 0, np.pi/2]])
-cup_pos = np.array([[0.4, -0.358, 0.47]])
-cup_ori = np.array([[0, 0, np.pi/2]])
+base_ori = np.array([[0, 0, np.pi / 2]])
+# final_pos = np.array([[0.4, -0.358, 0.47]])
+final_pos = np.array([[-0.0288, -1.1312, 0.4045]])
+final_ori = np.array([[np.pi / 2, np.pi / 2, 0]])
 t_o_0 = get_t_matrix(base_pos, base_ori)
-t_o_6 = get_t_matrix(cup_pos, cup_ori)
-
-# Distancia entre junta 6 e posicao da garra
-d6 = 0.075
+t_o_6 = get_t_matrix(final_pos, final_ori)
 
 # Gera angulos com Cinematica Inversa
-theta = ur5_ik(t_o_0, t_o_6, d6)
+theta_matrix = ur5_ik(t_o_0, t_o_6)
+
+print(f"\nTeste:\nfinal_pos = {final_pos}\nfinal_ori = {np.round_(final_ori, 4)}\n")
+print(f"theta_matrix = \n{np.round_(theta_matrix, 4)}\n")
+
+sim.startSimulation()
+tool = sim.getObject("/UR5/UR5_connection")
+joint_handles = reset_joints()
+sim.wait(2)
+
+
+for index, row in enumerate(theta_matrix):
+    configs = {
+        0: "ombro L, punho U, cotovelo U",
+        1: "ombro L, punho U, cotovelo D",
+        2: "ombro L, punho D, cotovelo U",
+        3: "ombro L, punho D, cotovelo D",
+        4: "ombro R, punho U, cotovelo U",
+        5: "ombro R, punho U, cotovelo D",
+        6: "ombro R, punho D, cotovelo U",
+        7: "ombro R, punho D, cotovelo D",
+    }
+    if np.isnan(row).any():
+        continue
+    else:
+        disp_row = np.round_(row, 4)
+        print(f"Configuracao {index}: {configs[index]}")
+        print(f"theta_calc = {disp_row.tolist()}")
+
+        set_pos(joint_handles, row)
+        sim.addLog(sim.verbosity_scriptinfos, f"test{index} = {disp_row}")
+        sim.wait(3)
+
+        tool_position = sim.getObjectPosition(tool, sim.handle_world)
+        print(f"p_desejado = {np.round_(final_pos, 4)}")
+        print(f"p_simulado = {np.round_(tool_position, 4)}\n")
 
 sim.stopSimulation()
