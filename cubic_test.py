11645ba97addbe73799d50814c0c452c742e89ad
Osmar Oliveira <osmar.oliveira@fbest.org.br>
2022-06-27 18:53:15 -0300

    accel changes

--- cubic_test.py
+++ cubic_test.py
@@ -12,18 +12,18 @@ ur5 = UR5_arm()
 sim = client.getObject('sim')
 client.setStepping(True)
 
-sim.startSimulation()
 
-def moveToAngle(target_angles, targetVels, joint_handles):
+
+def moveToAngle(target_angles, targetVels, joint_handles, target_force):
     done = [False for x in range(0, len(target_angles))]
     while (not all(done)):
         joint_angles = [sim.getJointPosition(x) for x in joint_handles]
         for idx, value in enumerate(target_angles):
-            if (abs(joint_angles[idx] - target_angles[idx]) > 0.1):
-                print(abs(joint_angles[idx] - target_angles[idx]))
-                print(targetVels[idx])
-                sim.setJointTargetVelocity(joint_handles[idx], targetVels[idx]*10)
-                sim.setJointMaxForce(joint_handles[idx], 100)
+            if (abs(joint_angles[idx] - target_angles[idx]) > 0.1 * math.pi / 180):
+                print(target_force[idx])
+                sim.setJointTargetPosition(joint_handles[idx], target_angles[idx])
+                sim.setJointTargetVelocity(joint_handles[idx], targetVels[idx])
+                sim.setJointMaxForce(joint_handles[idx], target_force[idx])
             else:
                 done[idx] = True
         client.step()
@@ -39,6 +39,7 @@ jointHandles = [
 currentConf = [ 0, 0, 0, 0, 0, 0 ]
 for x in jointHandles:
     sim.setJointTargetPosition(x, 0)
+    sim.setJointTargetVelocity(x, 2*math.pi)
 target_angles = [90, 0, 45, 45, 45, 60]
 targetConfig = [x*math.pi/180 for x in target_angles]
 matrix = ur5.forward_kinematic(*targetConfig)
@@ -53,8 +54,10 @@ for i, value in enumerate(targetConfig):
     vel.append(vel_temp)
     accel.append(accel_temp)
 
+sim.startSimulation()
+
 for idx, value in enumerate(pos[0]):
-    moveToAngle([v[idx] for v in pos], [v[idx] for v in vel], jointHandles)
+    moveToAngle([v[idx] for v in pos], [v[idx] for v in vel], jointHandles, [v[idx] for v in accel])
     print("step: " + str(idx))
 
 sim.stopSimulation()
